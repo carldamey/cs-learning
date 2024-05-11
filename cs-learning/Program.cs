818e099e5941577d19b328ed3da7552008f4b957
Carl <carldamey@gmail.com>
2024-05-10 19:46:17 -0700

    experiments further with variable types

--- cs-learning/Program.cs
+++ cs-learning/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 var napoleonVar = "Napoleon";
+int testInt = 15;
+double testflaot = 14.23432;
 Console.WriteLine(napoleonVar + " Concatenation");
